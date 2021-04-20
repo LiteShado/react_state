1530bc016c35537e97b7c131b557c4a601e777d6
Natalie <theliteshado@icloud.com>
2021-04-20 13:45:13 -0400

    color and counter changer

--- state/src/App.js
+++ state/src/App.js
@@ -1,10 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+    const [number, setNumber] = useState(0)
+    const [color, setColor] = useState('red')
+    const [showing, setShowing] = useState('color-changing')
+
   return (
+
     <div className="container">
-      
+
+      <nav>
+        <span onClick={() => { setShowing('colorchanger') }}>Color Changer</span>
+
+
+        <span onClick={() => { setShowing('counter') }}>Counter</span>
+      </nav>
+
+    { showing ==='colorchanger' ?
+
+    <div>
+      <div className= {`box ${color}`}/>
+      <div className="buttons-Container">
+      <button onClick={() => { setColor('red')}}>Red</button>
+      <button onClick={() => { setColor('blue')}}>Blue</button>
+      <button onClick={() => { setColor('green')}}>Green</button>
+      </div>
+    </div>
+
+    : null
+    }
+
+    { showing ==='counter' ?
+      <div>
+        <div className= {`box ${number}`}/>
+        <div className="buttons-Container">
+        <div>{number}</div>
+        <button onClick={() => { setNumber(number + 1) }}>+</button>
+        <button onClick={() => { setNumber(number - 1) }}>-</button>
+        </div>
+      </div>
+
+      : null
+
+    }
+
     </div>
   );
 }
